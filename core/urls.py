059c2aa75968d8b00d40a1056d4dd2cd10d3828d
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-11-13 05:51:21 +0100

    working on the set up for the codebase

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'^auth/', include('djoser.urls')),
+    path(r'^auth/', include('djoser.urls.jwt')),
+]
