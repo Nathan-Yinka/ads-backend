3cebe3f0e7e8f6d0f7562cc7ebb52000aef14dd7
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-11-23 12:39:54 +0100

    edited the date

--- core/settings.py
+++ core/settings.py
@@ -164,7 +164,7 @@ REST_FRAMEWORK = {
 
 "------------------------------- JSON Settings ------------------------------------------------------------------------"
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
